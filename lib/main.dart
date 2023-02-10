b348239fd0971e3f65c8a1d320c52664eef5b6b1
Mary Alrayes <maryalrayes@gmail.com>
2023-02-10 20:25:49 +0200

    first version

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: getApplicationThemeData(),
-      title: 'Task',
+      title: 'klshimol Task',
       home: const MainScreen(),
     );
   }
